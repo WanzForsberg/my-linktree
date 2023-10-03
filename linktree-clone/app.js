4f0446dffb95b6f176b682b9791b87d257ffb895
Jacob Forsberg <68010525+WanzForsberg@users.noreply.github.com>
2023-10-03 14:37:41 +0200

    Add files via upload

---
+++ linktree-clone/app.js
@@ -0,0 +1,15 @@
+const shareButtons = document.querySelectorAll('.tile-share-button');
+
+async function copyText(e) {
+  e.preventDefault();
+  const link = this.getAttribute('link');
+  try {
+    await navigator.clipboard.writeText(link);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+shareButtons.forEach((shareButton) =>
+  shareButton.addEventListener('click', copyText)
+);
